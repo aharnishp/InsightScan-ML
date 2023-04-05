700737db5bffe5dbab7f6262a8ed61a17061d71c
Aharnish <aharnishpithva@gmail.com>
2023-04-05 11:27:24 +0530

    extracted similar products in final-1

--- data-enhancer/get-similar-products.py
+++ data-enhancer/get-similar-products.py
@@ -19,25 +19,48 @@ with open("data-enhancer/chat-gpt-responses.txt", "w") as f:
 
 
 
-# Define your list of items to ask ChatGPT about
 
-items = ["Amul Butter", "Cadbury Dairy Milk Silk - Bubbly", "Haldiram's Moong Dal", "Maggi 2-Minute Noodles - Masala"]
+existingItems = ""
+# open file data-cleaning/dataset/sample.md and read the contents
+with open("data-cleaning/dataset/sample.md", "r") as f:
+    existingItems = f.read()
+
+# split the contents by newline
+existingItems = existingItems.split("\n")
+
+# # check if line has ". "
+# existingItems = [x for x in existingItems if ". " in x]
+
+
+newList = []
+
+for line in existingItems:
+    if(". " in line):
+        if(line.split(".")[0].isdigit()):
+            print(line)
+            print(line.split(".")[1])
+            newList.append(line.split(".")[1])
+
+existingItems = newList
+# strip whitespace
+existingItems = [x.strip() for x in existingItems]
+
+# remove empty strings
+existingItems = list(filter(None, existingItems))
+
+# remove duplicates
+existingItems = list(dict.fromkeys(existingItems))
+
+# print(existingItems)
+
+# Define your list of items to ask ChatGPT about
+items = existingItems
+# items = ["Amul Butter", "Cadbury Dairy Milk Silk - Bubbly", "Haldiram's Moong Dal", "Maggi 2-Minute Noodles - Masala"]
+# items = [] #["Amul Butter", "Cadbury Dairy Milk Silk - Bubbly", "Haldiram's Moong Dal", "Maggi 2-Minute Noodles - Masala"]
 
 # Loop through each item in the list and ask ChatGPT
 for item in items:
     # Define your prompt for ChatGPT
-    # prompt = "list the ingredients of the 10 food products which are similar to " + str(item)
-    # prompt = "for the product " + str(item) + ", list 10 food products which are similar to it and give all ingredients of each of them"
-    
-    # prompt = "get 10 food product brand names similar to the product " + str(item)  + " comma separated"
-    # prompt = "list the name of the 10 food products which are similar to " + str(item) + " comma separated"
-    # prompt = "Get comma separated list of 10 products like " + str(item)
-    # prompt = "Get comma separated list of 10 products like " + str(item)
-
-    # prompt = "Generate a list of branded food products that are similar to " + str(item) + ". Please include the product name, brand, and any relevant details such as flavor or packaging. Use your knowledge of the food industry and consumer preferences to generate the most relevant and up-to-date recommendations. "
-    # prompt = "Generate a list of branded food products that are similar to the following product: " + str(item) + ". Please provide the product name in the format 'Brand, Product Name', with a comma separating the brand and product name. Please include the product name, brand, and any relevant details such as flavor or packaging. Use your knowledge of the food industry and consumer preferences to generate the most relevant and up-to-date recommendations. "
-    # prompt = "Generate a comma-separated list of branded food products that are similar to " + str(item) + ". Include only the product name and brand, with no additional words or descriptions. Please use your knowledge of the food industry and consumer preferences to generate the most relevant and up-to-date recommendations."
-
     prompt = "List comma separated list of 10 food product names which are similar to the food product " + str(item)
 
     print("prompt: ", prompt)
